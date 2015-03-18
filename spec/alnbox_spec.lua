6afbf7202665013f151224fa190214ce73ec941f
Boris Nagaev <bnagaev@gmail.com>
2015-03-18 11:05:51 +0300

    add spec; simple test

---
+++ spec/alnbox_spec.lua
@@ -0,0 +1,27 @@
+local function startScript(rt, code)
+    if type(code) == 'function' then
+        code = string.dump(code)
+    end
+    local fname = os.tmpname()
+    local f = io.open(fname, 'w')
+    f:write(code)
+    f:close()
+    local cmd = 'lua %s; rm %s'
+    rt:forkPty(cmd:format(fname, fname))
+end
+
+describe("alnbox.alnbox", function()
+    it("draws simple alignment", function()
+        local rote = require 'rote'
+        local rt = rote.RoteTerm(24, 80)
+        startScript(rt, function()
+            local alnbox = require 'alnbox.alnbox'
+            alnbox {rows = 1, cols = 1,
+                getCell = function() return 'X' end}
+        end)
+        os.execute('sleep 5')
+        rt:update()
+        assert.truthy(rt:termText():match('X'))
+        rt:write('q')
+    end)
+end)
