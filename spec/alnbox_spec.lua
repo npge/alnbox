ec9e5f7ab4ef4ca8398fb8d2156a26566efa8d34
Boris Nagaev <bnagaev@gmail.com>
2015-03-18 13:02:35 +0300

    helper functions: startScript, startCode

--- spec/alnbox_spec.lua
+++ spec/alnbox_spec.lua
@@ -1,4 +1,16 @@
-local function startScript(rt, code)
+local function startScript(rt, lua_fname, remove)
+    local cmd
+    if remove then
+        cmd = 'lua -lluacov %s; rm %s'
+        cmd = cmd:format(lua_fname, lua_fname)
+    else
+        cmd = 'lua -lluacov %s'
+        cmd = cmd:format(lua_fname)
+    end
+    rt:forkPty(cmd)
+end
+
+local function startCode(rt, code)
     if type(code) == 'function' then
         code = string.dump(code)
     end
@@ -6,15 +18,15 @@ local function startScript(rt, code)
     local f = io.open(fname, 'w')
     f:write(code)
     f:close()
-    local cmd = 'lua -lluacov %s; rm %s'
-    rt:forkPty(cmd:format(fname, fname))
+    local remove = true
+    startScript(rt, fname, remove)
 end
 
 describe("alnbox.alnbox", function()
     it("draws simple alignment", function()
         local rote = require 'rote'
         local rt = rote.RoteTerm(24, 80)
-        startScript(rt, function()
+        startCode(rt, function()
             local alnbox = require 'alnbox.alnbox'
             alnbox {rows = 1, cols = 1,
                 getCell = function() return 'X' end}
