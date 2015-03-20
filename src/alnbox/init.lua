05db246860a87a6b7e4639e7d672d9debce1b93c
Boris Nagaev <bnagaev@gmail.com>
2015-03-20 11:02:00 -0400

    add copyright info to new modules

--- src/alnbox/init.lua
+++ src/alnbox/init.lua
@@ -1,3 +1,7 @@
+-- alnbox, alignment viewer based on the curses library
+-- Copyright (C) 2015 Boris Nagaev
+-- See the LICENSE file for terms of use
+
 return {
     alnbox = require 'alnbox.alnbox',
     cursesConsts = require 'alnbox.cursesConsts',
