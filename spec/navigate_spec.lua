d492d38156b6bec0f13699f019cc0d1b4fd007d5
Boris Nagaev <bnagaev@gmail.com>
2015-03-22 07:50:33 -0400

    navigate: key SPACE repeats last action 15 times

--- spec/navigate_spec.lua
+++ spec/navigate_spec.lua
@@ -5,22 +5,27 @@
 describe("alnbox.navigate", function()
     it("calls method moveUp when a user presses UP", function()
         local cursesConsts = require 'alnbox.cursesConsts'
+        local times_up = 0
         local aw = {
             drawAll = function() end,
-            moveUp = function() end,
+            moveUp = function()
+                times_up = times_up + 1
+            end,
         }
         local refresh = function() end
         local getch = coroutine.wrap(function()
             coroutine.yield(cursesConsts.KEY_UP)
+            coroutine.yield(string.byte(' '))
             coroutine.yield(string.byte('q'))
         end)
         --
-        stub(aw, "drawAll")
-        stub(aw, "moveUp")
+        spy.on(aw, "drawAll")
+        spy.on(aw, "moveUp")
         --
         local navigate = require 'alnbox.navigate'
         navigate(aw, refresh, getch)
-        assert.stub(aw.drawAll).was_called()
-        assert.stub(aw.moveUp).was_called()
+        assert.spy(aw.drawAll).was_called()
+        assert.spy(aw.moveUp).was_called()
+        assert.truthy(times_up >= 2)
     end)
 end)
--- src/alnbox/navigate.lua
+++ src/alnbox/navigate.lua
@@ -8,17 +8,23 @@ return function(aw, refresh, getch, bindings)
 
     aw:drawAll()
 
+    local last_action
+
     while true do
         refresh()
         local ch = getch()
         if ch == cursesConsts.KEY_UP then
             aw:moveUp()
+            last_action = aw.moveUp
         elseif ch == cursesConsts.KEY_DOWN then
             aw:moveDown()
+            last_action = aw.moveDown
         elseif ch == cursesConsts.KEY_RIGHT then
             aw:moveRight()
+            last_action = aw.moveRight
         elseif ch == cursesConsts.KEY_LEFT then
             aw:moveLeft()
+            last_action = aw.moveLeft
         elseif ch == cursesConsts.KEY_PPAGE then
             aw:moveUpEnd()
         elseif ch == cursesConsts.KEY_NPAGE then
@@ -29,6 +35,10 @@ return function(aw, refresh, getch, bindings)
             aw:moveLeftEnd()
         elseif ch == string.byte('q') then
             break
+        elseif ch == string.byte(' ') and last_action then
+            for i = 1, 15 do
+                last_action(aw)
+            end
         elseif bindings and bindings[ch] then
             local func = bindings[ch]
             func()
