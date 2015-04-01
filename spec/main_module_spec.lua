7a12f9af071917a32980805bd227c00a34430c0b
Boris Nagaev <bnagaev@gmail.com>
2015-04-01 13:57:42 +0300

    add pending test for main module
    This test fails with the folowing error message:
    
    > ./spec/main_module_spec.lua:9:
    >    attempt to index local 'alnbox' (a function value)

---
+++ spec/main_module_spec.lua
@@ -0,0 +1,11 @@
+-- alnbox, alignment viewer based on the curses library
+-- Copyright (C) 2015 Boris Nagaev
+-- See the LICENSE file for terms of use
+
+describe("alnbox", function()
+    pending("provides all submodules as fields", function()
+        local alnbox = require 'alnbox'
+        local makeAlignment = require 'alnbox.makeAlignment'
+        assert.equal(makeAlignment, alnbox.makeAlignment)
+    end)
+end)
